2f211059bc7f23ad683d6693736f6e4a6ee1f5d0
Felix Gabriel Martinez Casadiego <felix.martinez@auraxtechnology.com>
2025-12-23 20:38:46 +0100

    first commit

---
+++ 01-Bases/Variables.cs
@@ -0,0 +1,11 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int age=25;
+        string name ="Alice";
+        bool isStudent = true;
+
+        Console.WriteLine($"Name: {name}, Age: {age}, Is Student: {isStudent}");
+    }
+}
