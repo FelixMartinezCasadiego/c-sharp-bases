3765c9330c7843d6fc26df1528a087b622d322c8
Felix Gabriel Martinez Casadiego <felix.martinez@auraxtechnology.com>
2026-01-01 13:44:02 +0100

    feat: implement Bus and Fleet classes with methods for managing buses; update Main to demonstrate fleet functionality

--- 03-Classes/Homework-01.cs
+++ 03-Classes/Homework-01.cs
@@ -9,6 +9,17 @@ partial class Program
         inventory.AddProduct(mouse);
         inventory.ShowInvetory();
     }
+
+    static void BusFleet()
+    {
+        Fleet fleet = new();
+        Bus bus1 = new(brand: "Volvo", model: "7700", year: 2015, price: 120000, totalKilometers: 150000);
+        Bus bus2 = new(brand: "Mercedes", model: "Citaro", year: 2018, price: 150000, totalKilometers: 80000);
+        fleet.AddBus(bus1);
+        fleet.AddBus(bus2);
+        bus1.Drive(500);
+        fleet.ShowFleet();
+    }
 }
 
 class Product(string name, double price, int stock)
@@ -50,6 +61,44 @@ class Inventory
         foreach (var product in products)
         {
             product.ShowInfo();
+        }
+    }
+}
+
+class Bus(string brand, string model,int year, double price, int totalKilometers)
+{
+    public string? Brand { get; set; } = brand;
+    public string? Model { get; set; } = model;
+    public int Year { get; set; } = year;
+    public double Price { get; set; } = price;
+    public int TotalKilometers { get; set; } = totalKilometers;
+
+    public void Drive(int kilometers)
+    {
+        TotalKilometers += kilometers;
+    }
+
+    public double ShowPrice() {
+        return Price;
+    }
+}
+
+class Fleet
+{
+    private readonly List<Bus> buses = [];
+
+    public void AddBus(Bus bus)
+    {
+        buses.Add(bus);
+    }
+    
+    public void ShowFleet()
+    {
+        WriteLine("Fleet:");
+
+        foreach (var bus in buses)
+        {
+            WriteLine($"Bus: {bus.Brand} {bus.Model}, Year: {bus.Year}, Price: {bus.Price}, Total Kilometers: {bus.TotalKilometers}");
         }
     }
 }
